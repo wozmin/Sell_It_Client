5e6e22a6e11f1707148aa7d8ed77052b2266c6f0
Andriy Ikavetc <wozm1nnt@gmail.com>
2019-05-15 23:57:06 +0300

    add share social buttons for realty details

--- src/app/realty/realty.module.ts
+++ src/app/realty/realty.module.ts
@@ -15,9 +15,38 @@ import {library} from '@fortawesome/fontawesome-svg-core';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {faHome} from '@fortawesome/free-solid-svg-icons/faHome';
 import {faBuilding} from '@fortawesome/free-solid-svg-icons/faBuilding';
+import {faFacebookF} from '@fortawesome/free-brands-svg-icons/faFacebookF';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons/faTwitter';
+import {faRedditAlien} from '@fortawesome/free-brands-svg-icons/faRedditAlien';
+import {faLinkedinIn} from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
+import {faGooglePlusG} from '@fortawesome/free-brands-svg-icons/faGooglePlusG';
+import {faTumblr} from '@fortawesome/free-brands-svg-icons/faTumblr';
+import {faPinterestP} from '@fortawesome/free-brands-svg-icons/faPinterestP';
+import {faWhatsapp} from '@fortawesome/free-brands-svg-icons/faWhatsapp';
+import {faVk} from '@fortawesome/free-brands-svg-icons/faVk';
+import {faFacebookMessenger} from '@fortawesome/free-brands-svg-icons/faFacebookMessenger';
+import {faTelegramPlane} from '@fortawesome/free-brands-svg-icons/faTelegramPlane';
+import {faMix} from '@fortawesome/free-brands-svg-icons/faMix';
+import {faXing} from '@fortawesome/free-brands-svg-icons/faXing';
+import {faLine} from '@fortawesome/free-brands-svg-icons/faLine';
+
+import {faCommentAlt} from '@fortawesome/free-solid-svg-icons/faCommentAlt';
+import {faMinus} from '@fortawesome/free-solid-svg-icons/faMinus';
+import {faEllipsisH} from '@fortawesome/free-solid-svg-icons/faEllipsisH';
+import {faLink} from '@fortawesome/free-solid-svg-icons/faLink';
+import {faExclamation} from '@fortawesome/free-solid-svg-icons/faExclamation';
+import {faPrint} from '@fortawesome/free-solid-svg-icons/faPrint';
+import {faCheck} from '@fortawesome/free-solid-svg-icons/faCheck';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import {ShareButtonsModule} from '@ngx-share/buttons';
 import {fas} from '@fortawesome/free-solid-svg-icons';
 
-library.add(fas, faHome, faBuilding);
+const icons = [
+  faFacebookF, faTwitter, faLinkedinIn, faGooglePlusG, faPinterestP, faRedditAlien, faTumblr,
+  faWhatsapp, faVk, faFacebookMessenger, faTelegramPlane, faMix, faXing, faCommentAlt,
+  faEnvelope, faCheck, faPrint, faExclamation, faLink, faEllipsisH, faMinus, faLine
+];
+library.add(fas, faHome, faBuilding, ...icons);
 
 @NgModule({
   declarations: [RealtyListComponent, RealtyDetailsComponent, RealtyEditComponent, RealtyFormComponent, RealtyAddComponent],
@@ -29,7 +58,10 @@ library.add(fas, faHome, faBuilding);
     MatProgressSpinnerModule,
     NgxGalleryModule,
     FileDropModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ShareButtonsModule.withConfig({
+      windowWidth: 150
+    })
   ]
 })
 export class RealtyModule { }
