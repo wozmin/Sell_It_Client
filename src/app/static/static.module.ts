7296e199d9aec9ede63b483c9ef0579976f4a7b3
Andrii Ikavetc <aikavetc@netfully.com>
2019-05-20 17:00:26 +0300

    implement localization for static module

--- src/app/static/static.module.ts
+++ src/app/static/static.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import {NotFoundComponent} from "./not-found/not-found.component";
 import {SharedModule} from "../shared/shared.module";
 import {RouterModule} from "@angular/router";
+import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   declarations: [NotFoundComponent],
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
-
+    RouterModule,
+    TranslateModule.forChild()
   ],
   exports:[NotFoundComponent]
 
