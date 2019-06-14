4bed9aa4dbd54b5f6a3a44922a8274ef3456cbb4
Andrii Ikavetc <aikavetc@netfully.com>
2019-06-14 15:11:44 +0300

    add node compress

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 const app = express();
-
+app.use(compression());
 // Serve static files....
-app.use(express.static(__dirname + '/dist/SellItClient/build/'));
+app.use(express.static(__dirname + '/dist/SellItClient/'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/SellItClient/build/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/SellItClient/index.html'));
 });
 
 // default Heroku PORT
